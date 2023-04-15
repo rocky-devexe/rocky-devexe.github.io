26e3e805c707bf0113c422cc5a70135c68dad11d
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2023-04-15 19:09:30 +0200

    v4.2.1
    -new download thing 
    -removed yellow 1000000 pixels long rectangle
    -fixed bugs
    -and #### version number because i don't like numbers and versions 1.0.0 to 2.0.0 to 3.0.0 to 4.0.0 to 4.2.1

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function bd() { 
-	console.log("Hello World") 
-}
+var menu = document.getElementById("a0");
+//var body = document.getElementById("mb");
+var hello = document.getElementById("logos");//.innerHTML = '<div class="menu" id="mu"><div class="ms" id="ms"></div></div>';
+hello.innerHTML = '<div class="menu" id="mu"><div class="ms" id="ms"></div></div>'
+menu.addEventListener('click', () => {
+  menu.innerHTML = '<div class="a">Button Clicked!</div>';
+  window.setTimeout(() => {
+    menu.innerHTML = '<div class="a"><img src="https://cdn-icons-png.flaticon.com/512/7216/7216128.png" alt="download" width="40" height="40"></div>';
+  }, 2000);
+});
---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\hi\rocky-devexe.github.io\index.html
+echo compiler
