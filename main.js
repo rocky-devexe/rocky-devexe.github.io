ccee9fb2db4eb4b604c8bc9dcaada46f7743ce4d
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2023-05-02 21:17:22 +0200

    v5.1.0
    redesign almost all sites beta option menu and more

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var menu = document.getElementById("a0");
-//var body = document.getElementById("mb");
-var hello = document.getElementById("logos");//.innerHTML = '<div class="menu" id="mu"><div class="ms" id="ms"></div></div>';
-hello.innerHTML = '<div class="menu" id="mu"><div class="ms" id="ms"></div></div>'
-const a = document.getElementsByClassName("i");
+var balls = document.getElementById("c");
+var ball = document.getElementById("d");
 menu.addEventListener('click', () => {
-  menu.innerHTML = '<div class="a">Button Clicked!</div>';
-  window.setTimeout(() => {
-    menu.innerHTML = '<div class="a"><img src="https://cdn-icons-png.flaticon.com/512/7216/7216128.png" alt="download" width="40" height="40"></div>';
-  }, 2000);
+	if (balls.style.display === "none") {
+		balls.style.display = "block";
+	} else {
+		 balls.style.display = "none";
+	}
+});
+ball.addEventListener('click', () => {
+	balls.style.display = "none";
 });
