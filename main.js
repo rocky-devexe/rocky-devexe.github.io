60882a324b74c1c79141743d8a10ff73fc3945fd
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2023-05-25 21:09:19 +0200

    v5.1.2
    implemented real dark mode
    redesign the parts of website and a.css

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var menu = document.getElementById("a0");
+var a = document.getElementById("a1");
+var b = document.getElementById("a2");
+var c = document.getElementById("a3");
+var d = document.getElementById("a4");
 var balls = document.getElementById("c");
 var ball = document.getElementById("d");
 menu.addEventListener('click', () => {
@@ -11,8 +15,21 @@ menu.addEventListener('click', () => {
 ball.addEventListener('click', () => {
 	balls.style.display = "none";
 });
+a.addEventListener('click', () => {
+	location.href = 'download.html';
+});
+b.addEventListener('click', () => {
+	location.href = 'about.html';
+});
+c.addEventListener('click', () => {
+	location.href = 'support.html';
+});
+d.addEventListener('click', () => {
+	location.href = 'https://github.com/rocky-devexe/rocky-devexe.github.io';
+});
 var tall = window.innerHeight;
 var wide = window.innerWidth;
+var a = document.getElementById("mb");
 var x = 720;
 var y = 1280;
 console.log("Height: ",tall)
@@ -20,5 +37,5 @@ console.log("Width: ",wide)
 console.log("X: ",x)
 console.log("Y: ",y)
 if (wide <= x && tall <= y) {
-	console.log("Decteck Mobile Phone Phone mode Enabled")
+	a.innerHTML = '<div class="a">';
 }
