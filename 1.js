cee2e599fb5efed0a4b6fd821a56007128ea81ae
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2023-06-20 13:37:29 +0200

    5.1.4
    ...

---
+++ 1.js
@@ -0,0 +1,6 @@
+var menu2 = document.getElementById("owo"); 
+var e = document.getElementById("ishjdhsfsdkjgj");
+e.addEventListener('click', () => {
+	menu2.style.display = "block";
+	e.innerHTML = '';
+});
