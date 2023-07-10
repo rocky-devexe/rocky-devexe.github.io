71cdeb88f78d6a1a81fe2d25aae66bc6fceac65c
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2023-07-10 17:06:44 +0200

    Delete 1.js
    unused file gets deleted

--- 1.js
+++
@@ -1,6 +0,0 @@
-var menu2 = document.getElementById("owo"); 
-var e = document.getElementById("ishjdhsfsdkjgj");
-e.addEventListener('click', () => {
-	menu2.style.display = "block";
-	e.innerHTML = '';
-});
