e0af1fc38c71a96aae37f9a61a6204c0d5606857
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2024-12-23 13:31:32 +0100

    Delete scripts directory

--- scripts/doomget64.sh
+++
@@ -1,8 +0,0 @@
-#dont froget to chmod +x doomget32.sh and to ./doomget32.sh or sh doomget32.sh
-apt install unzip
-apt install wget
-wget https://archive.org/download/2020_03_22_DOOM/DOOM%20WADs/Doom%20%28v1.9%29.zip
-wget https://zdoom.org/files/zdoom/2.8/zdoom_2.8.1_amd64.deb
-echo dpkg --install zdoom-2.8.2-amd64.deb
-echo unzip the doom wad with the command "unzip"
-echo you need to put the wad into the game
