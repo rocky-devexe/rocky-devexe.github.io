c251869f39be761c447a0450ff59176735a8a6ac
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2024-12-23 13:32:42 +0100

    Delete mobile.js

--- mobile.js
+++
@@ -1,8 +0,0 @@
-var tall = window.innerHeight;
-var wide = window.innerWidth;
-var a = document.getElementById("mb");
-var x = 730;
-var y = 1280;
-if (wide <= x && tall <= y) {
-	location.href = 'mobile/mobile_index.html';
-}
